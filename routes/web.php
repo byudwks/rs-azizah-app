d79a76db819f1e8bc2015596b27ee78f5e044a13
byudwks <bayudwikiswanto24@gmail.com>
2025-09-20 12:47:49 +0700

    o

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,3 @@ use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-
-Route::get('/run-migrations', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return 'Migrations run successfully';
-});
